bab21f6eb9fbdf2b8c458dfe9e683b340662f1cb
erin0210 <shakiraharina0210@gmail.com>
2020-07-27 12:42:08 +0800

    removed unused imports

--- lib/screens/list.dart
+++ lib/screens/list.dart
@@ -1,4 +1,4 @@
-import 'package:beauty_app/screens/home.dart';
+//import 'package:beauty_app/screens/home.dart';
 import 'package:flutter/material.dart';
 import 'package:beauty_app/util/data.dart';
 
